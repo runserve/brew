9e4f6218b71a672df144cc6ca72ae1b63b0772f2
mirek <mirek.grzegorczyk@gmail.com>
2019-02-25 11:23:46 +0100

    v0.0.2

--- runserve.rb
+++ runserve.rb
@@ -3,7 +3,7 @@ class Runserve < Formula
   homepage https://runserve.io
   version '0.0.2'
   url 'https://github.com/runserve/runserve/archive/v0.0.2.tar.gz'
-  sha256 0712ac92d60c52aca2be411e811c3ecca44553296c8741816729e455f254fbc0
+  sha256 '0712ac92d60c52aca2be411e811c3ecca44553296c8741816729e455f254fbc0'
 	def install
 		bin.install 'runserve'
 	end
