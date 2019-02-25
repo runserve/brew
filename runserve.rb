7086642420d2026f43ae4f2fab0d3aa495da3d81
mirek <mirek.grzegorczyk@gmail.com>
2019-02-25 11:26:44 +0100

    v0.0.2

--- runserve.rb
+++ runserve.rb
@@ -1,6 +1,6 @@
 class Runserve < Formula
   desc 'NodeJS Docker Development Environment'
-  homepage https://runserve.io
+  homepage 'https://runserve.io'
   version '0.0.2'
   url 'https://github.com/runserve/runserve/archive/v0.0.2.tar.gz'
   sha256 '0712ac92d60c52aca2be411e811c3ecca44553296c8741816729e455f254fbc0'
