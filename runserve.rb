bc1ee806184973699f211052bc2c18699e988b72
Mirek Grzegorczyk <mirek@dnd-usb>
2019-03-17 09:33:58 +0100

    v0.1.0

--- runserve.rb
+++ runserve.rb
@@ -1,9 +1,9 @@
 class Runserve < Formula
   desc 'NodeJS Docker Development Environment'
   homepage 'https://runserve.io'
-  version '0.0.16'
-  url 'https://github.com/runserve/runserve/archive/v0.0.16.tar.gz'
-  sha256 'a96bbb538d0a090032f5a5ab17e85985db3ffe937196b471e39c7c1b9103e5cb'
+  version '0.1.0'
+  url 'https://github.com/runserve/runserve/archive/v0.1.0.tar.gz'
+  sha256 'da2b54a95c686cce492aefcb47e1e6bc9b1451edab7db69f855312c1c0809bc8'
 	def install
 		bin.install 'runserve'
 	end
